0efd0c606ee567c747014f4b4f76753df2d0441b
rami13-meet <rami15manna@gmail.com>
2016-04-21 22:51:29 -0400

    applying example to make translate work

--- example.js
+++ example.js
@@ -7,7 +7,21 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
-
+    ext.get_translation = function(text, target_language, callback) {
+      var language = {"Afrikaans":"af", "Albanian" : "sq",
+"Arabic" : "ar", "Armenian" : "hy", "Azerbaijani" :"az", "Basque"  :"eu", "Belarusian" : "be", "Bengali": "bn", "Bosnian" :"bs","Bulgarian": "bg","Catalan" :"ca","Cebuano" :"ceb","Chichewa" : "ny","Chinese Simplified" : "zh-CN","Chinese Traditional": "zh-TW","Croatian" : "hr","Czech": "cs","Danish"  :"da","Dutch" :"nl","English" :"en","Esperanto" :"eo","Estonian" : "et","Filipino" : "tl","Finnish" :"fi","French" : "fr","Galician" : "gl","Georgian" : "ka","German" : "de","Greek": "el","Gujarati" : "gu","Haitian Creole": "ht","Hausa" :"ha","Hebrew":  "iw","Hindi": "hi","Hmong" :"hmn","Hungarian": "hu","Icelandic": "is","Igbo" : "ig","Indonesian" : "id","Irish": "ga","Italian" :'it',"Japanese" : "ja","Javanese" : "jw","Kannada" :"kn","Kazakh" : "kk","Khmer": "km","Korean" : "ko","Lao": "lo","Latin" : "la","Latvian" : "lv","Lithuanian" : "lt","Macedonian" : "mk","Malagasy" : "mg","Malay" :"ms","Malayalam" :"ml","Maltese" :"mt","Maori" :"mi","Marathi": "mr","Mongolian" :"mn","Myanmar (Burmese)": "my","Nepali" : "ne","Norwegian": "no","Persian" :"fa","Polish"  :"pl","Portuguese"  :"pt","Punjabi" :"ma","Romanian" : "ro","Russian": "ru","Serbian" :"sr","Sesotho" :"st","Sinhala" :"si","Slovak" : "sk","Slovenian" :"sl","Somali" : "so","Spanish": "es","Sudanese" : "su","Swahili" :"sw","Swedish": "sv","Tajik" :"tg","Tamil" :"ta","Telugu" : "te","Thai" : "th","Turkish" :"tr","Ukrainian" : "uk","Urdu" : "ur","Uzbek" :"uz","Vietnamese"  :"vi","Welsh" :"cy","Yiddish" : "yi","Yoruba" : "yo","Zulu" : "zu"};        
+        var sourceText =  text;
+      }      
+        // Make an AJAX call to the Open Weather Maps API
+        $.ajax({
+              url: 'https://www.googleapis.com/language/translate/v2?key=AIzaSyCRXDflkdRiEhQ-JLMwzuSsq2O55aWZ8-0&source=en&target='+language[target_language]+'&callback=translateText&q=' + sourceText;
+              dataType: 'jsonp',
+              success: function( source ) {
+                  // Got the data - parse it and return the temperature
+                  newScript.src = source;
+                  callback(newScript);
+              }
+        });
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
@@ -19,8 +33,14 @@
           ['h', 'when distance %m.lessMore %n',          'whenDistance', '<', 20],
           ['h', 'when tilt %m.eNe %n',                   'whenTilt', '=', 1],
           ['r', 'distance',                              'getDistance'],
-          ['r', 'tilt',                                  'getTilt']
+          ['r', 'tilt',                                  'getTilt'],
+          ['r', '%s in %m.languages',                    'get_translation','hello world','Spanish']
+        menus: {
+            languages: ['Spanish','Arabic','Hebrew'],
+            lessMore: ['<', '>'],
+            eNe: ['=','not =']
         ]
+
     };
 
     // Register the extension
